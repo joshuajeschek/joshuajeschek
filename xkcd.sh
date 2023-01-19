416f707acdc6791a3d3125ec0fbebee5f8997469
Joshua Jeschek <joshua@jeschek.eu>
2023-01-19 15:43:10 +0100

    feat: auto xkcd comics

---
+++ xkcd.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+SRC=$(curl -s https://xkcd.com/info.0.json | jq -r '.img')
+ALT=$(curl -s https://xkcd.com/info.0.json | jq -r '.alt')
+
+sed -i -e 's|<img id="xkcd" [^>]* width|<img id="xkcd" src="'"${SRC}"'" alt="'"${ALT}"'" width|g' README.md
+
