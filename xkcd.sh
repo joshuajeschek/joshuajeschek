487b4a5860caabc1992e6b7b11c5fcec8331253e
Joshua Jeschek <joshua@jeschek.eu>
2023-01-19 15:48:00 +0100

    fix: commit step and script info

--- xkcd.sh
+++ xkcd.sh
@@ -2,5 +2,8 @@
 SRC=$(curl -s https://xkcd.com/info.0.json | jq -r '.img')
 ALT=$(curl -s https://xkcd.com/info.0.json | jq -r '.alt')
 
+echo "SRC: $(SRC)"
+echo "ALT: $(ALT)"
+
 sed -i -e 's|<img id="xkcd" [^>]* width|<img id="xkcd" src="'"${SRC}"'" alt="'"${ALT}"'" width|g' README.md
 
